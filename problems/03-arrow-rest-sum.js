b62a6c74fd04bcf4e954427ee50b3dbc073aded9
Joshua Nobleza <joshnobleza@bell.net>
2022-06-01 12:12:17 -0400

    finished arrowRestSum function

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -10,7 +10,7 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// your code here!
+const arrowRestSum = (...args) => args.reduce((accum, el) => accum + el, 0);
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
